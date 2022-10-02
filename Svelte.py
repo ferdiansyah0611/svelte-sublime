4546a5d1bfd35dd41c9b0cda6f5df7cfeae43936
Ferdiansyah <ferdif9996@gmail.com>
2022-10-02 17:06:11 +0700

    [bug]

--- Svelte.py
+++ Svelte.py
@@ -6,21 +6,22 @@ class SvelteCommand(sublime_plugin.EventListener):
 	
 	def on_query_completions(self, view, prefix, locations):
 		target = html_svelte
-		if not view.match_selector(locations[0], "text.html.svelte"):
-			if not view.match_selector(locations[0], "source.javascript"):
-				return []
-			else:
-				target = non_svelte
+		is_svelte = view.match_selector(locations[0], "text.html.svelte")
+		is_js = view.match_selector(locations[0], "source.js")
+
+		if is_svelte == False and is_js == False: return []
+		if is_js: target = non_svelte
 
 		prefix 	= prefix.lower()
 		out 	= []
 		cursor 	= locations[0] - len(prefix) - 1
 		line   	= view.substr(sublime.Region(0, cursor))
 		split  	= line.split('\n')
-		is_html = '</script>' in split
 
-		if is_html == False:
-			target = js_svelte
+		if is_svelte:
+			is_html = '</script>' in split
+			if is_html == False:
+				target = js_svelte
 
 		for comp in target:
 			if comp.trigger.startswith(prefix):
